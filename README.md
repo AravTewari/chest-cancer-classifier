98ac453f7dc874d3841134b4ceb337eee546c635
Arav Tewari <aravtewari@gmail.com>
2024-11-04 15:06:06 -0500

    add README and requirements.txt

---
+++ README.md
@@ -0,0 +1,2 @@
+# Cancer classifer using CNNs
+Deployed with MLFlow and DVC on AWS
--- template.py
+++ template.py
@@ -24,18 +24,14 @@ list_of_files = [
     "setup.py",
     "research/trials.ipynb",
     "templates/index.html"
-
-
 ]
-
 
 
 for filepath in list_of_files:
     filepath = Path(filepath)
     filedir, filename = os.path.split(filepath)
 
-
-    if filedir !="":
+    if filedir != "":
         os.makedirs(filedir, exist_ok=True)
         logging.info(f"Creating directory; {filedir} for the file: {filename}")
 
@@ -43,7 +39,6 @@ for filepath in list_of_files:
         with open(filepath, "w") as f:
             pass
             logging.info(f"Creating empty file: {filepath}")
-
 
     else:
         logging.info(f"{filename} is already exists")
