d1079d6a0dd5614e334f3acb3a2b39fd163f3aaa
Arav Tewari <aravtewari@gmail.com>
2024-11-04 20:52:06 -0500

    added model training pipeline

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_03_model_trainer import ModelTrainingPipeline
 
 STAGE_NAME = "Data ingestion"
 try:
@@ -20,5 +21,16 @@ try:
     prepare_base_model.run()
     logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
-        logger.exception(e)
-        raise e
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = 'Model training'
+try:
+    logger.info(f"*******************")
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = ModelTrainingPipeline()
+    obj.run()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/cnnClassifier/pipeline/stage_03_model_trainer.py
@@ -0,0 +1,31 @@
+
+from cnnClassifier.components.model_trainer import Training
+from cnnClassifier.config.configuration import ConfigurationManager
+from cnnClassifier import logger
+
+
+STAGE_NAME = 'Model trainer'
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def run(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        trainer = Training(training_config)
+        trainer.get_base_model()
+        trainer.make_train_valid_generators()
+        trainer.train()
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = ModelTrainingPipeline()
+        obj.run()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
